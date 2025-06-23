5275874e6b8d01a8159ae074c999d48802673e91
消逝中的寒光 <2507544221@qq.com>
2025-06-23 20:13:10 +0800

    配置类实现

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+import yaml
+
+
+# 自动读取config.yaml的配置
+class Config:
+    def __init__(self, config_file="config.yaml"):
+        config_path = os.path.join(os.path.dirname(__file__), config_file)
+        with open(config_path, "r", encoding="utf-8") as f:
+            self._config = yaml.safe_load(f)
+
+    # 获取配置项，支持嵌套访问(智能返回字符串或字典)
+    def get(self, key_path, default=None, sep="."):
+        keys = key_path.split(sep)
+        data = self._config
+        for key in keys:
+            if isinstance(data, dict):
+                data = data.get(key, default)
+            else:
+                return default
+        return data
---
+++ spider/school.py
@@ -0,0 +1,11 @@
+class School:
+    def __init__(self):
+        self.url = 'https://yz.chsi.com.cn/zsml/rs/dws.do'
+        self.form_data = {
+            'dwmc': '',
+            'dwdm': '',
+            'xxfs': '',
+            'tydxs': '',
+            'jsggjh': '',
+        }
+
